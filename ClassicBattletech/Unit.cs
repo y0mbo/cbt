e5e31153f18932e2d16b638d0023b4dd531bb756
John Uhri <john.uhri@redbitbluebit.com>
2024-04-06 16:09:18 -0500

    Created the Game object

---
+++ ClassicBattletech/Unit.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ClassicBattletech;
+
+abstract class Unit
+{
+    public abstract string Name { get; }
+}
